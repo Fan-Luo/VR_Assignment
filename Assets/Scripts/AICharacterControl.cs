fb2bcdf5bcc1567e8dff165f7f5253ae445590b0
Fan-Luo <luofan813@gmail.com>
2019-03-13 10:16:03 -0700

    NavMesh and moving beams

---
+++ Assets/Scripts/AICharacterControl.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using UnityEngine;
+
+namespace UnityStandardAssets.Characters.ThirdPerson
+{
+    [RequireComponent(typeof(UnityEngine.AI.NavMeshAgent))]
+    [RequireComponent(typeof(ThirdPersonCharacter))]
+    public class AICharacterControl : MonoBehaviour
+    {
+        public UnityEngine.AI.NavMeshAgent agent { get; private set; }             // the navmesh agent required for the path finding
+        public ThirdPersonCharacter character { get; private set; } // the character we are controlling
+        public Transform target;                                    // target to aim for
+        private Ray rayFromMainCamera; // ray from main camera
+        private RaycastHit hitFromRay; // the point where the ray hits
+        public Texture targetTexture; // target texture
+        private Transform target2; // second target for green circle
+        public Texture target2Texture;// second target texture 
+        private void Start()
+        {
+            // get the components on the object we need ( should not be null due to require component so no need to check )
+            agent = GetComponentInChildren<UnityEngine.AI.NavMeshAgent>();
+            character = GetComponent<ThirdPersonCharacter>();
+            agent.updateRotation = false;
+            agent.updatePosition = true;
+            target.position = new Vector3(0f, 0f, -8f);
+        }
+
+
+        private void Update()
+        {
+            rayFromMainCamera = new Ray(Camera.main.transform.position, Camera.main.transform.rotation * new Vector3(0f, 0f, 1f));// ray from main camera starts at main camera's position and goes forward to where the camera is rotated
+            if (Physics.Raycast(rayFromMainCamera, out hitFromRay)) // checks if the ray interesects with a collider
+            {
+                if (hitFromRay.collider.gameObject.name == "Ground" && Input.anyKeyDown) // if the ray hits the ground
+                {
+                    target.position = hitFromRay.point; //target position = hit point position
+                    target.GetComponent<Renderer>().material.SetTexture("_MainTex", targetTexture); // sets target's texture to targetTexture
+                    target.GetComponent<Renderer>().material.color = new Color(1f, 0.92f, 0.016f, .5f); // 
+                    if (Input.anyKeyDown)
+                    {
+
+                    }
+                }
+            }
+
+            if (target != null)
+                agent.SetDestination(target.position);
+
+            if (agent.remainingDistance > agent.stoppingDistance)
+                character.Move(agent.desiredVelocity, false, false);
+            else
+                character.Move(Vector3.zero, false, false);
+        }
+
+
+        public void SetTarget(Transform target)
+        {
+            this.target = target;
+        }
+    }
+}
---
+++ Assets/Scripts/movingBeam.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movingBeam : MonoBehaviour {
+
+    public GameObject beam1;
+    public GameObject beam2;
+    public GameObject beam3;
+    public static float speed;
+    int direction1 = 1;
+    int direction2 = 1;
+    int direction3 = 1;
+    // Use this for initialization
+    void Start () {
+        speed = Random.Range(2.0f, 5.0f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        //beam1
+        if(beam1.transform.position.y > 4.0f || beam1.transform.position.y < 0.0f)
+        {
+            direction1 *= -1;
+        }
+        beam1.transform.Translate(0.0f, speed * Time.deltaTime * direction1, 0.0f);
+
+
+        //beam2
+        if (beam2.transform.position.x > 4.0f || beam2.transform.position.x < -4.0f)
+        {
+            direction2 *= -1;
+        }
+        beam2.transform.Translate(3 * speed * Time.deltaTime * direction2, 0.0f, 0.0f);
+
+        //beam3
+        if(beam3.transform.position.y > 4.0f || beam3.transform.position.y < 0.0f)
+        {
+            direction3 *= -1;
+        }
+        beam3.transform.Translate(0.0f, 2 * speed * Time.deltaTime * direction3, 0.0f);
+
+
+        //If there’s a hit with Ethan’s collider
+
+
+    }
+}
